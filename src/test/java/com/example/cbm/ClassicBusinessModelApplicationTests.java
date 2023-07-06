eecae9788198dc6cc6ed44c00ed81bd74ebfff6c
shanmukh786 <134614042+shanmukh786@users.noreply.github.com>
2023-07-06 22:00:00 +0530

    Update ClassicBusinessModelApplicationTests.java

--- src/test/java/com/example/cbm/ClassicBusinessModelApplicationTests.java
+++ src/test/java/com/example/cbm/ClassicBusinessModelApplicationTests.java
@@ -3,10 +3,9 @@ package com.example.cbm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ClassicBusinessModelApplicationTests {
-
-	@Test
+// @Test
 	void contextLoads() {
 	}
 
